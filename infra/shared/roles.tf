64eb04663627b2e8de9765b9acdc550d7053cc47
Nicolas Sauvage <nicolas.sauvage@talan.com>
2021-06-08 11:50:51 +0200

    WIP

--- infra/shared/roles.tf
+++ infra/shared/roles.tf
@@ -4,8 +4,16 @@ resource "aws_iam_role" "ecsTaskRole" {
   assume_role_policy = file("${path.module}/resources/ecsTaskRole.json")
 }
 
+data aws_iam_policy task_execution_policy {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name = "bookadeskEcsTaskExecutionRole"
-
   assume_role_policy = file("${path.module}/resources/ecsTaskExecutionRole.json")
+}
+
+resource "aws_iam_role_policy_attachment" "sto-readonly-role-policy-attach" {
+  role = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = data.aws_iam_policy.task_execution_policy.arn
 }
