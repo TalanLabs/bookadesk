5d951b0d55b7ccfa2cb9dddb8aa44ed0f925c92b
Nicolas Sauvage <nicolas.sauvage@talan.com>
2021-07-02 17:47:35 +0200

    fix compilation

--- back/src/adapters/InMemoryBookingRepo.ts
+++ back/src/adapters/InMemoryBookingRepo.ts
@@ -37,4 +37,8 @@ export class InMemoryBookingRepo implements BookingRepo {
   confirmPresence(): Promise<void> {
     throw new Error("Method not implemented.");
   }
+
+  getPlaceBookingsAfterDate(): Promise<Booking[]> {
+    throw new Error("Method not implemented.");
+  }
 }
