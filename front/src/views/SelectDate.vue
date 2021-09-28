d5e3cee4052ebbf90be7026e456111b3c97614d6
Nicolas Sauvage <nicolas.sauvage@talan.com>
2021-09-28 21:45:17 +0200

    add date picker

---
+++ front/src/views/SelectDate.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Datepicker
+      v-model="selectedDate"
+      maximum-view="day"
+      :inline="true"
+      :disabled-dates="disabledDates"
+      :language="fr"
+      :monday-first="true"
+      :full-month-name="true"
+      @selected="onSelect"
+    ></Datepicker>
+  </div>
+</template>
+<script lang="ts">
+import Datepicker from "vuejs-datepicker";
+import { add } from "date-fns";
+import { fr } from "vuejs-datepicker/dist/locale";
+
+export default {
+  name: "SelectDate",
+  components: { Datepicker },
+  data() {
+    return {
+      selectedDate: new Date(),
+      disabledDates: {
+        to: add(new Date(), { days: -1 }),
+        from: add(new Date(), { months: 1 })
+      },
+      fr: fr
+    };
+  },
+  methods: {
+    onSelect(d: Date) {
+      this.$emit("change", d);
+    }
+  }
+};
+</script>
